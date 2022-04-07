13407027fcef190d341c060e404a1e4e6317b05c
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-07 13:55:20 +0700

    Refactoring code dan menambah beberapa fitur command line

--- simple-contact-app/app.js
+++ simple-contact-app/app.js
@@ -1,16 +1,40 @@
-const { addQuestion, saveContact } = require('./contacts');
+const yargs = require('yargs');
+const contacts = require('./contacts');
 
-const mainFunc = async () => {
-    const nama = await addQuestion('Masukkan nama Anda : ');
-    const email = await addQuestion('Masukkan alamat email Anda : ');
-    const telp = await addQuestion('Masukkan nomor telepon Anda : ');
+// Command untuk menambah contact baru kedalam json
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan data contact baru',
+    builder: {
+        nama: {
+            describe: 'Nama lengkap',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Alamat email',
+            demandOption: false,
+            type: 'string',
+        },
+        noTelp: {
+            describe: 'Nomor telepon',
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler(argv) {
+        contacts.saveContact(argv.nama, argv.email, argv.noTelp);
+    }
+}).demandCommand();
 
-    saveContact(nama, email, telp);
-} 
+// Command untuk melihat seluruh daftar nama dan nomor handphone contact
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan seluruh data nama dan nomor telepon pada contact',
+    handler() {
+        contacts.listContact();
+    }
+})
 
-mainFunc();
 
-
-
-
- 
+yargs.parse();
--- simple-contact-app/contacts.js
+++ simple-contact-app/contacts.js
@@ -1,14 +1,6 @@
-// File System
 const fs = require('fs');
-
-// Readline
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
+const chalk = require('chalk');
+const validator = require('validator');
 // membuat folder data
 const dirPath = './data';
 if ( !fs.existsSync(dirPath) ) {
@@ -21,35 +13,49 @@ if ( !fs.existsSync(filePath) ) {
     fs.writeFileSync(filePath, '[]', 'utf-8');
 }
 
-// Menulsikan file kedalam format json dengan metode promise dan async await
-const addQuestion = (question) => {
-    return new Promise((resolve, reject) => {
-        rl.question(question, (nama) => {
-            resolve(nama);
-        }); 
-    });
+const loadContact = () => {
+    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(fileBuffer);
+
+    return contacts;
 }
 
 const saveContact =  (nama, email, telp) => {
     const contact = { nama, email, telp };
-    const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
-    const contacts = JSON.parse(fileBuffer);
+    const contacts = loadContact();
+
+    const duplicate = contacts.find((contact) => contact.nama === nama);
+
+    if ( duplicate ) {
+        console.log(chalk.bgRed.black.bold('Contact sudah terdaftar, harap coba lagi'));
+        return false;
+    }
+
+    if ( email ) {
+        if ( !validator.isEmail(email) ) {
+            console.log(chalk.bgRed.black.bold('Email tidak valid, harap masukkan email yang sesuai!'));
+            return false;
+        }
+    }
+
+    if ( !validator.isMobilePhone(telp, 'id-ID') ) {
+        console.log(chalk.bgRed.black.bold('Nomor telepon tidak valid, cek kembali apakah nomor telepon Anda itu sesuai dengan ketentuan!'));
+        return false;
+    }
 
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    console.log(`Terima kasih sudah menginputkan data! silakan coba lagi jika berkenan`);
-
-    rl.close(); 
+    console.log(chalk.green.inverse.bold(`Terima kasih sudah menginputkan data! silakan coba lagi jika berkenan`));
 }
 
-module.exports = { addQuestion, saveContact };
+const listContact = () => {
+    const contacts = loadContact();
+    console.log(chalk.blue.inverse(`DAFTAR CONTACT :`));
+    contacts.forEach((contact, i) => {
+        console.log(`${i + 1}. ${contact.nama} - ${contact.telp}`);
+    });
+}
 
-// const question2 = () => {
-//     return new Promise((resolve, reject) => {
-//         rl.question('Inputkan email address Anda : ', (email) => {
-//             resolve(email);
-//         }); 
-//     });
-// }
+module.exports = { listContact, saveContact };
