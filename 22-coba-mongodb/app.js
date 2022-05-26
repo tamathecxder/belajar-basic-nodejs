9fc55a498daa16081a50024583740e1f38e96e43
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-26 18:59:43 +0700

    Mencoba mamasukkan 1 field data kedalam mongodb dengan script node.js

--- 22-coba-mongodb/app.js
+++ 22-coba-mongodb/app.js
@@ -1,9 +1,33 @@
 const { MongoClient } = require('mongodb');
 
 const uri = 'mongodb://127.0.0.1:27017';
-
 const dbName = 'expressdb';
 
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+client.connect((err, client) => {
+    if (err) {
+        return console.log(err);
+    }
+
+    // pilih database
+    const db = client.db(dbName);
+
+    // menambahkan 1 data ke collection mahasiswa
+    db.collection('mahasiswa').insertOne({
+        nama: 'Maulana',
+        email: 'maulana@gmail.com'
+    }, ((err, result) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log(result.ops);
+    })); 
+});
 
 
 
