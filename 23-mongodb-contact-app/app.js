1489e690496c663bb777f1c6911d9255236c8f53
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-28 19:05:27 +0700

    Setup untuk masing masing pages

--- 23-mongodb-contact-app/app.js
+++ 23-mongodb-contact-app/app.js
@@ -1,39 +1,57 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 
-const app = express()
+const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 // Homepage
-app.get('/', (req, res) => {
-  const mahasiswa = [
-      {
-          nama: "Ajun Bagas",
-          email: "ajunbagas@gmail.com"
-      },
-      {
-          nama: "Sarah Apriliani",
-          email: "sarahapr@gmail.com"
-      },
-      {
-          nama: "Gustav Kennedy",
-          email: "gustavken@gmail.com"
-      }
+app.get("/", (req, res) => {
+  const pegawai = [
+    {
+      nama: "Ajun Bagas",
+      email: "ajunbagas@gmail.com",
+    },
+    {
+      nama: "Sarah Apriliani",
+      email: "sarahapr@gmail.com",
+    },
+    {
+      nama: "Gustav Kennedy",
+      email: "gustavken@gmail.com",
+    },
   ];
 
-  res.render('index', {
-      layout: 'layouts/main-layout',
-      title: "Homepage",
-      email: "ujangs@yandex.com",
-      pegawai: pegawai
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Homepage",
+    email: "ujangs@yandex.com",
+    pegawai: pegawai,
   });
-}); 
 
-app.listen(port, () => {
-  console.log(`Mongo Contact App | Listening at http://localhost:${port}`)
+  app.get("/about", (req, res) => {
+    res.render("about", {
+      layout: "layouts/main-layout",
+      title: "About page",
+    });
+  });
+
+  app.get("/contact", (req, res) => {
+    const contacts = loadContact();
+
+    res.render("contact", {
+      layout: "layouts/main-layout",
+      title: "Contact page",
+      contacts: contacts,
+      msg: req.flash("msg"),
+    });
+  });
 });
 
-
-
-
-
+app.listen(port, () => {
+  console.log(`Mongo Contact App | Listening at http://localhost:${port}`);
+});
