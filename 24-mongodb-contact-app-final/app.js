d28b611c0ec3707ea57c19ed4c62bb68312026ab
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-29 10:30:57 +0700

    Fix error pada root url

--- 24-mongodb-contact-app-final/app.js
+++ 24-mongodb-contact-app-final/app.js
@@ -65,85 +65,86 @@ app.get("/", (req, res) => {
       title: "About page",
     });
   });
+});
 
-  app.get("/contact", async (req, res) => {
-    // Menggunakan promise, tapi datanya belum tampil sempurna
-    // Contact.find.then((contact) => {
-    //   res.send(contact)
-    // });
+app.get("/contact", async (req, res) => {
+  // Menggunakan promise, tapi datanya belum tampil sempurna
+  // Contact.find.then((contact) => {
+  //   res.send(contact)
+  // });
 
-    const contacts = await Contact.find();
+  const contacts = await Contact.find();
 
-    res.render("contact", {
-      layout: "layouts/main-layout",
-      title: "Contact page",
-      contacts: contacts,
-      msg: req.flash("msg"),
-    });
+  res.render("contact", {
+    layout: "layouts/main-layout",
+    title: "Contact page",
+    contacts: contacts,
+    msg: req.flash("msg"),
   });
+});
 
-  // Halaman form add data contact
-  app.get("/contact/add", (req, res) => {
-    res.render("add-contact", {
-      layout: "layouts/main-layout",
-      title: "Form tambah data contact",
-    });
+// Halaman form add data contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    layout: "layouts/main-layout",
+    title: "Form tambah data contact",
   });
+});
 
-  // Proses tambah data contact
-  app.post(
-    "/contact",
-    [
-      body("nama").custom((value) => {
-        const duplicate = checkDuplicate(value);
-        if (duplicate) {
-          throw new Error(
-            "Nama contact sudah tersedia, harap gunakan nama lain!"
-          );
-        }
+// Proses tambah data contact
+app.post(
+  "/contact",
+  [
+    body("nama").custom(async (value) => {
+      const duplicate = await Contact.findOne({ nama: value });
+      if (duplicate) {
+        throw new Error(
+          "Nama contact sudah tersedia, harap gunakan nama lain!"
+        );
+      }
 
-        return true;
-      }),
-      check("email", "Alamat email tidak valid!").isEmail(),
-      check("telp", "Nomor telepon tidak valid!").isMobilePhone("id-ID"),
-    ],
-    (req, res) => {
-      const errors = validationResult(req);
+      return true;
+    }),
+    check("email", "Alamat email tidak valid!").isEmail(),
+    check("telp", "Nomor telepon tidak valid!").isMobilePhone("id-ID"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
-      if (!errors.isEmpty()) {
-        // return res.status(400).json({ errors: errors.array() });
-        res.render("add-contact", {
-          layout: "layouts/main-layout",
-          title: "Form tambah data contact",
-          errors: errors.array(),
-        });
-      } else {
-        addContact(req.body);
+    if (!errors.isEmpty()) {
+      // return res.status(400).json({ errors: errors.array() });
+      res.render("add-contact", {
+        layout: "layouts/main-layout",
+        title: "Form tambah data contact",
+        errors: errors.array(),
+      });
+    } else {
+      Contact.insertMany(req.body, (error, result) => {
         // Kirimkan flash message
         req.flash("msg", "Data contact berhasil ditambahkan");
         res.redirect("/contact");
-      }
+      });
     }
-  );
+  }
+);
 
-  // Halaman detail contact
-  app.get("/contact/:nama", async (req, res) => {
-    const contact = await Contact.findOne({ nama: req.params.nama });
+// Halaman detail contact
+app.get("/contact/:nama", async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
 
-    res.render("detail", {
-      layout: "layouts/main-layout",
-      error: "Contact dengan nama tersebut tidak tersedia!",
-      title: "Detail contact",
-      contact: contact,
-    });
+  res.render("detail", {
+    layout: "layouts/main-layout",
+    error: "Contact dengan nama tersebut tidak tersedia!",
+    title: "Detail contact",
+    contact: contact,
   });
+});
 
-  // Halaman form add data contact
-  app.get("/contact/add", (req, res) => {
-    res.render("add-contact", {
-      layout: "layouts/main-layout",
-      title: "Form tambah data contact",
-    });
+// Halaman form add data contact
+app.get("/contact/add", (req, res) => {
+  res.render("add-contact", {
+    layout: "layouts/main-layout",
+    title: "Form tambah data contact",
   });
 });
 
